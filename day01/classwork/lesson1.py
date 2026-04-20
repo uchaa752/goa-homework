4256830a5477114dc3a98fe322f8ce045c2a11b6
uchaa752 <xundazeahmed@gmail.com>
2026-04-21 00:43:26 +0400

    commit

---
+++ day01/classwork/lesson1.py
@@ -0,0 +1,9 @@
+print("ucha tabatadze")
+
+name="ucha tabatadze"
+
+print(name)
+
+age="14"
+
+print(age)
---
+++ day01/homework/lesson1.py
@@ -0,0 +1,15 @@
+name="ucha"
+
+print(name)
+
+lastname="tabatadze"
+
+print("lastname")
+
+age="14"
+
+print(age)
+
+favouritecollor="dark blue"
+
+print(favouritecollor)
