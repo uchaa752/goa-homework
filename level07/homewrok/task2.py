2d4c8bc795ce1a962f866de59a0233e47ca76849
uchaa752 <xundazeahmed@gmail.com>
2026-05-14 15:31:05 +0400

    commit

---
+++ level07/homewrok/task2.py
@@ -0,0 +1,18 @@
+numbers = []
+
+num1 = int(input("enter your number: "))
+numbers.append(num1)
+
+num2 = int(input("enter your number: "))
+numbers.append(num2)
+
+num3 = int(input("enter your number: "))
+numbers.append(num3)
+
+num4 = int(input("enter your number: "))
+numbers.append(num4)
+
+num5 = int(input("enter your number: "))
+numbers.append(num5)
+
+print(numbers)
